bad8e9d258ed970e7b536bd3d9f905e34706b86d
Roy <royxrodriguez@gmail.com>
2019-07-25 15:49:17 -0400

    add main, home, product components, associated tests, and initial redux store

--- server/main.js
+++ server/main.js
@@ -1,23 +1,24 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const path = require('path');
+const router = require('./routes')
 
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
-};
+}
 
 const app = express();
 
 //the use of this router must come from routes
-app.use("/api", router);
+app.use('/api', router);
 
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const categories = require('./categories')
 const productRoutes = require("./product.js");
 
-router.use("/api/product", productRoutes);
+router.use('/products', productRoutes);
+
 router.use('/categories', categories)
 
 module.exports = router;
