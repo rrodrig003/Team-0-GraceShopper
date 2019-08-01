18595724a4eedc1016b4d6b56ee529afe63a209a
Kevin <kyu199023@gmail.com>
2019-08-01 19:32:59 -0400

    Import redirect

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Navbar from './Navbar';
