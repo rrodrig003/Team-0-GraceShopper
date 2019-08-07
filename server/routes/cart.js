2019e6e5a578cbf332b46b78a2d25bcb092a3eac
Ashton <asch8216@gmail.com>
2019-08-07 15:44:07 -0400

    updated naming of routes

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -5,7 +5,7 @@ const OrderItem = require('../database/models/orderItem.js');
 // order table and add row to orderItem
 
 // finding an order where the id matches the sessionId. gives the orders associated with the sessionId
-router.get('/:sessionId', async (req, res, next) => {
+router.get('/session/:sessionId', async (req, res, next) => {
   try {
     const order = await Order.findOne({
       where: {
@@ -24,7 +24,7 @@ router.get('/:sessionId', async (req, res, next) => {
 });
 
 // returns order based on orderId being passed in. Finds out which items belongs to cart
-router.get('/:cart/orderId', async (req, res, next) => {
+router.get('/order/:orderId', async (req, res, next) => {
   try {
     const order = await OrderItem.findAll({
       where: {
@@ -58,7 +58,7 @@ router.get('/user/:userId', async (req, res, next) => {
 });
 
 // add to cart route
-router.post('/:orderId', async (req, res, next) => {
+router.post('/order/:orderId', async (req, res, next) => {
   try {
     // orderId, productId comes from req.body. quantity one by default
     const orderId = parseInt(req.body.orderId, 10);
@@ -71,55 +71,5 @@ router.post('/:orderId', async (req, res, next) => {
     next(e);
   }
 });
-
-// delete from cart route
-router.delete('/', async (req, res, next) => {
-  try {
-    const orderId = parseInt(req.body.orderId, 10);
-    const productId = parseInt(req.body.productId, 10);
-    const item = await OrderItem.findOne({
-      where: {
-        orderId,
-        productId,
-      },
-    });
-    res.send(item.destroy());
-  } catch (e) {
-    next(e);
-  }
-});
-
-// update quantity of items in cart with put route
-
-router.put('/', async (req, res, next) => {
-  try {
-    const orderId = parseInt(req.body.orderId, 10);
-    const productId = parseInt(req.body.productId, 10);
-    const quantity = parseInt(req.body.quantity, 10);
-    const item = await OrderItem.findOne({
-      where: {
-        orderId,
-        productId,
-      },
-    })
-      // res.send(item.update(quantity));
-      .then((prod) => {
-        prod.update(quantity);
-      });
-  } catch (e) {
-    next(e);
-  }
-});
-
-// router.put('/', (req, res, next) => {
-//   OrderItem.findOne({
-//     where: req.body,
-//   })
-//     .then((foundItem) => {
-//       foundItem.destroy();
-//     })
-//     .then(() => res.status(204).send('Found and deleted'))
-//     .catch(next);
-// });
 
 module.exports = router;
