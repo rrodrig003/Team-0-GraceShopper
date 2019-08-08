759229fc7b81098769714ec3544e0bb7a10b5686
Kevin <kyu199023@gmail.com>
2019-08-08 12:21:01 -0400

    Require order route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ router.use('/auth', require('./auth'));
 router.use('/products', require('./product'));
 router.use('/categories', require('./categories'));
 router.use('/cart', require('./cart'));
+router.use('/order', require('./order'));
 
 module.exports = router;
