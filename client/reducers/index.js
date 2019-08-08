83fed3ffe659e003300692b0b4bda6adbd488fdc
Roy <royxrodriguez@gmail.com>
2019-08-08 14:58:43 -0400

    cart component v1

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import products from './products';
 import category from './category';
 import authenticate from './auth';
+import cart from './cart';
 
 const reducer = combineReducers({
   authenticate,
   products,
   category,
+  cart,
 });
 
 export default reducer;
